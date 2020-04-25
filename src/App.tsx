fcb7571e830ae6e3a6e87dfaecb92edb66f9918a
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-04-25 16:21:45 +0200

    chore: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <Provider>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
